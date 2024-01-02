f50a8781efd24d3c952d01ae356b6392a0a9c7c5
UshanKaushalya <Ushanwk22@gmail.com>
2024-01-02 23:26:17 +0530

    hero section done

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const App = () => (
 
         <Nav/>
 
-        <section className="xl:padding-1 wide:padding-r padding-b">
+        <section className='xl:padding-l wide:padding-r padding-b'>
             <Hero/>
         </section>
 
