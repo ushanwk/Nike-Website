dfb19a13e8c1edb4d72bb3a99ff40adfc583bde8
UshanKaushalya <Ushanwk22@gmail.com>
2024-01-02 23:32:47 +0530

    our popular products section developing

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,6 +1,12 @@
 const PopularProducts = () => {
     return(
-        <div>PopularProducts</div>
+        <section id="products" className="max-container max-sm:mt-12">
+            <div className="flex flex-col justify-start gap-5">
+                <h2 className="text-4xl font-palanquin font-bold">Our <span className="text-coral-red">Popular</span> Products</h2>
+                <p> Experience top-notch quality and style with our sought-after
+                    selections. Discover a world of comfort, design, and value</p>
+            </div>
+        </section>
     )
 }
 
