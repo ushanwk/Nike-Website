6053bfc2480caff3bc47fe53108156e41ea520ee
UshanKaushalya <Ushanwk22@gmail.com>
2024-01-02 15:05:35 +0530

    hero are developing

---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+const Button = () => {
+    return(
+        <button className="">
+            Click me
+        </button>
+    )
+}
+
+export default Button;
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,21 @@
+import Button from "../components/Button.jsx";
+
 const Hero = () => {
     return(
-        <div>Hero</div>
+        <section id="home" className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container">
+
+            <div className="relative xl:w-2/5 flex flex-col justify-cente items-start w-full max-xl:padding-x pt-28">
+                <p>Our Summer Collection</p>
+                <h1><
+                    span>The New Arrival</span>
+                    <br/>
+                    <span>Nike </span>Shoes
+                </h1>
+                <p>Discover stylish Nike arrivals, quality comfort, and innovations for your active life.</p>
+                <Button/>
+            </div>
+
+        </section>
     )
 }
 
