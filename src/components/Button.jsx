c594f2bf4d4d06eeda57b229d7a18e467a879557
UshanKaushalya <Ushanwk22@gmail.com>
2024-01-03 00:20:51 +0530

    super quality section developing

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ const Button = ({ label, iconUrl}) => {
     return(
         <button className="flex justify-center items-center gap-2 px-7 py-4 font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
             {label}
-            <img className="ml-2 rounded-full w-5 h-5" src={iconUrl}/>
+            {iconUrl && <img className="ml-2 rounded-full w-5 h-5" src={iconUrl}/>}
         </button>
     )
 }
--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,6 +1,28 @@
+import Button from "../components/Button.jsx";
+
 const SuperQuality = () => {
     return(
-        <div>SuperQuality</div>
+        <section id="about-us" className="flex justify-between items-center mx-lg:flex-col gap-10 w-full max-container">
+            <div className="flex flex-1 flex-col">
+                <h2 className='font-palanquin text-4xl capitalize font-bold lg:max-w-lg'>
+                    We Provide You
+                    <span className="text-coral-red"> Super </span>
+                    <span className="text-coral-red">Quality </span>
+                    Shoes
+                </h2>
+                <p className='mt-4 lg:max-w-lg info-text'>
+                    Ensuring premium comfort and style, our meticulously crafted footwear
+                    is designed to elevate your experience, providing you with unmatched
+                    quality, innovation, and a touch of elegance.
+                </p>
+                <p className='mt-6 lg:max-w-lg info-text'>
+                    Our dedication to detail and excellence ensures your satisfaction
+                </p>
+                <div className='mt-11'>
+                    <Button label='View details'/>
+                </div>
+            </div>
+        </section>
     )
 }
 
