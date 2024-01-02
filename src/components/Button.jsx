6e16f391b8608cd7fe42c8e4e6a1759d229efb85
UshanKaushalya <Ushanwk22@gmail.com>
2024-01-02 22:13:01 +0530

    hero are developing

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,8 @@
-const Button = () => {
+const Button = ({ label, iconUrl}) => {
     return(
-        <button className="">
-            Click me
+        <button className="flex justify-center items-center gap-2 px-7 py-4 font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
+            {label}
+            <img className="ml-2 rounded-full w-5 h-5" src={iconUrl}/>
         </button>
     )
 }
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,20 +1,32 @@
 import Button from "../components/Button.jsx";
+import {arrowRight} from "../assets/icons/index.js";
+import {statistics} from "../constants/index.js";
 
 const Hero = () => {
     return(
         <section id="home" className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container">
-
             <div className="relative xl:w-2/5 flex flex-col justify-cente items-start w-full max-xl:padding-x pt-28">
-                <p>Our Summer Collection</p>
-                <h1><
-                    span>The New Arrival</span>
-                    <br/>
-                    <span>Nike </span>Shoes
-                </h1>
-                <p>Discover stylish Nike arrivals, quality comfort, and innovations for your active life.</p>
-                <Button/>
-            </div>
+                <p className="text-xl font-montserrat text-coral-red">Our Summer Collection</p>
 
+                <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold">
+                    <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">The New Arrival</span>
+                    <br/>
+                    <span className="text-coral-red inline-block mt-3">Nike </span>Shoes
+                </h1>
+
+                <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">Discover stylish Nike arrivals, quality comfort, and innovations for your active life.</p>
+
+                <Button label="Shop now" iconUrl={arrowRight}/>
+
+                <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
+                    {statistics.map((stat) => (
+                        <div key={stat.label}>
+                            <p className="text-4xl font-palanquin font-bold">{stat.value}</p>
+                            <p className="leading-7 font-montserrat text-slate-gray">{stat.label}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </section>
     )
 }
