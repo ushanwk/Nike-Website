f57d8e8ffe9b0f8953011d9cd2a9a19e9869f3f0
UshanKaushalya <Ushanwk22@gmail.com>
2024-01-04 00:52:45 +0530

    super quality section done

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,11 @@
-const Button = ({ label, iconUrl}) => {
+const Button = ({ label, iconUrl, backgroundColor, borderColor, textColor, fullWidth }) => {
     return(
-        <button className="flex justify-center items-center gap-2 px-7 py-4 font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
+        <button className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+      ${
+            backgroundColor
+                ? `${backgroundColor} ${textColor} ${borderColor}`
+                : "bg-coral-red text-white border-coral-red"
+        } rounded-full ${fullWidth && "w-full"}`}>
             {label}
             {iconUrl && <img className="ml-2 rounded-full w-5 h-5" src={iconUrl}/>}
         </button>
